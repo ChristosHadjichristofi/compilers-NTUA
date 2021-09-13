40d4fc1b0fb2cb0bc3b2df59fa7ab98c872e4052
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-09-13 14:10:50 +0300

    fix nan error on lib functions
    still not working:
    1. pi, float_of_int
    2. test24 (deref x has a problem)
    
    add to run.sh codeGen folder

--- compiler/run.sh
+++ compiler/run.sh
@@ -13,6 +13,7 @@ echo "3. semCorrect/other"
 echo "4. semCorrect/types"
 echo "5. typeMismatch"
 echo "6. warnings"
+echo "7. codeGen"
 echo ""
 read opt
 echo ""
@@ -29,18 +30,27 @@ elif [ $opt -eq 5 ]; then
   directory="examples/typeMismatch"
 elif [ $opt -eq 6 ]; then
   directory="examples/warnings"
+elif [ $opt -eq 7 ]; then
+  directory="examples/codeGen"
+  codeGen=true
 fi
 
+if [ "$codeGen" = true ] ; then
+  for file in $directory/*.lla; do
+    cd src && make && ./do.sh "../"$file > "../"$file.txt && cd ..
+  done
+else
 
-echo "Do you want to use diff.py? [true/false]"
-read diff
+  echo "Do you want to use diff.py? [true/false]"
+  read diff
 
-echo "Where is your git repo folder located? "
-read folder
+  echo "Where is your git repo folder located? "
+  read folder
 
-for file in $directory/*.lla; do
-  cd src && make && ./llama < "../"$file > "../"$file.txt && cd ..
-  if "$diff" == true; then
-    python3 $HOME/$folder/compilers-NTUA/compiler/diff.py $file $HOME $folder
-  fi
-done
+  for file in $directory/*.lla; do
+    cd src && make && ./llama < "../"$file > "../"$file.txt && cd ..
+    if "$diff" == true; then
+      python3 $HOME/$folder/compilers-NTUA/compiler/diff.py $file $HOME $folder
+    fi
+  done
+fi
